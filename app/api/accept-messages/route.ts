2c78e4ad48c2ee3fe22e545367c24f0c535df58d
Prathamesh Kulkarni <prathameshkulkarni22092004@gmail.com>
2026-03-08 21:30:02 +0530

    feat: add GET endpoint to retrieve user's message acceptance status

--- app/api/accept-messages/route.ts
+++ app/api/accept-messages/route.ts
@@ -62,3 +62,52 @@ export async function POST(request: Request) {
     }
 
 }
+
+export async function GET(request: Request) {
+    await dbConnect()
+
+    const session = await getServerSession(authOptions)
+    const user = session?.user as User
+
+    if (!session || !session.user) {
+        return Response.json(
+            {
+                success: false,
+                message: "Not authenticated"
+            },
+            { status: 401 }
+        )
+    }
+
+    const userId = user._id;
+
+    try {
+        const foundUser = await UserModel.findById(userId)
+        if (!foundUser) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "failed to found the user "
+                },
+                { status: 404 }
+            )
+        }
+
+        return Response.json(
+            {
+                success: true,
+                isAcceptingMessages: foundUser.isAcceptingMessages
+            },
+            { status: 200 }
+        )
+    } catch (error) {
+        console.error("failed to find user status to accept message");
+        return Response.json(
+            {
+                success: false,
+                message: "Error in getting message accepting status"
+            },
+            { status: 500 }
+        )
+    }
+}
